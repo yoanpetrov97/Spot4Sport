8c4f4c79d6d396e4ea52a6a1f17625b7da4a74aa
hstoyanovv <h.stoyanov44@gmail.com>
2021-07-06 13:16:24 +0300

    4 playgrounds were added

--- app/src/main/java/com/spot4sport/app/LoadDatabase.java
+++ app/src/main/java/com/spot4sport/app/LoadDatabase.java
@@ -18,19 +18,31 @@ class LoadDatabase {
     CommandLineRunner initCourtDatabase(CourtRepository repository) {
 
         return args -> {
-            log.info("Preloading " + repository.save(new Court("Зала \"Христо Ботев\"",
+            log.info("Preloading " + repository.save(new Court("Спортна зала \"Христо Ботев\"",
                   "1 зала за волейбол",
                   "ж.к. \"Студентски град\"")));
             log.info("Preloading " + repository.save(new Court("Спортна зала \"Fireball\"",
                   "1 зала за волейбол, 2 бадминтон корта, 1 зала за скуош, 4 тенис маси",
                   "Цариградско шосе 133, БИЦ-ИЗОТ корпус 4")));
 
-            log.info("Preloading " + repository.save(new Court("Спортен център \"Денис Белинов\"",
-                  "1 зала за баскетбол",
-                  "ж.к. не трябваше да ни изоставяш")));
-            log.info("Preloading " + repository.save(new Court("Някаква зала \"Еди коя си\"",
+            log.info("Preloading " + repository.save(new Court("Спортен център \"Dream body\"",
+                  "1 зала за баскетбол, 1 зала за футбол",
+                  "Черни връх 11")));
+            log.info("Preloading " + repository.save(new Court("Спортен център \"Европа\"",
                   "3 игрища за футбол, 1 игрище за баскетбол, 2 тенис корта",
-                  "Някъде в София, примерно")));
+                    "ж.к. \"Стрелбище\", ул. Нишава 129")));
+            log.info("Preloading " + repository.save(new Court("Спортна зала \"Раковски\"",
+                    "1 игрище за футбол, 1 игрище за баскетбол, 1 тенис корт",
+                    "бул. България 5")));
+            log.info("Preloading " + repository.save(new Court("Спортен център \"Лозенец\"",
+                    "2 игрища за волейбол, 1 игрище за баскетбол, 4 тенис корта, 1 игрище за бадминтон",
+                    "бул. Джеймс Баучер 9")));
+            log.info("Preloading " + repository.save(new Court("Спортен център \"Кристал\"",
+                    "1 игрище за футбол, 2 игрища за баскетбол, 2 игрища за бадминтон",
+                    "бул. Ломско шосе 209")));
+            log.info("Preloading " + repository.save(new Court("Спортен център \"Актив\"",
+                    "1 игрище за футбол, 1 игрище за баскетбол, 1 тенис корт",
+                    "ул. Солун 59Б")));
         };
     }
 }
--- app/src/main/java/com/spot4sport/app/model/Court.java
+++ app/src/main/java/com/spot4sport/app/model/Court.java
@@ -8,15 +8,42 @@ import java.util.Objects;
 @Entity
 public class Court {
 
-    private @Id
-    @GeneratedValue
-    Long id;
+    private @Id @GeneratedValue Long id;
     private String name;
     private String playgrounds;
     private String address;
     //TODO Add location(probably should be a class)
 
-    public Court() {
+    public String getPlaygrounds() {
+        return playgrounds;
+    }
+
+    @Override
+    public String toString() {
+        return "Court{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", playgrounds='" + playgrounds + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public void setPlaygrounds(String playgrounds) {
+        this.playgrounds = playgrounds;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Court() {}
+
+    public Court(String name) {
+        this.name = name;
     }
 
     public Court(String name, String playgrounds, String address) {
@@ -33,14 +60,6 @@ public class Court {
         return name;
     }
 
-    public String getPlaygrounds() {
-        return playgrounds;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -49,37 +68,18 @@ public class Court {
         this.name = name;
     }
 
-    public void setPlaygrounds(String description) {
-        this.playgrounds = description;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Court)) return false;
         Court court = (Court) o;
         return getId().equals(court.getId()) &&
-              getName().equals(court.getName()) &&
-              getPlaygrounds().equals(court.getPlaygrounds()) &&
-              getAddress().equals(court.getAddress());
+                getName().equals(court.getName());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getName(), getPlaygrounds(), getAddress());
+        return Objects.hash(getId(), getName());
     }
 
-    @Override
-    public String toString() {
-        return "Court{" +
-              "id=" + id +
-              ", name='" + name + '\'' +
-              ", playgrounds='" + playgrounds + '\'' +
-              ", address='" + address + '\'' +
-              '}';
-    }
 }
